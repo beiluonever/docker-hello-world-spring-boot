8a5346c731d2eec6484a8dd4e769890dd6cd0336
beiluonever <beiluonever@qq.com>
2023-02-15 15:50:31 +0800

    add memory and sleep for thread and memory dump

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -4,11 +4,23 @@ package com.dockerforjavadevelopers.hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
     public String index() {
+        try {
+            String[] a = new String[10000000];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = "x" + i;
+            }
+            System.out.println(a.length);
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "Hello World\n";
     }
     
